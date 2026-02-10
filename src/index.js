fb5f349c2f33519e54f744741aba08898f019301
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-02-10 23:00:50 +0530

    utils files added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import 'dotenv/config'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 
 // dotenv.config({
 //     path:'../env'
 // });
 
-connectDB()
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error - ",error);
+        process.exit(1)
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Express is running at PORT : ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("Database connection failed :(",err);
+    process.exit(1)
+})
