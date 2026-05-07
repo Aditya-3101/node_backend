72ba13cfc973251dcfca900fa9d1d6964556b22f
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-05-08 01:34:50 +0530

    vercel fix

--- src/index.js
+++ src/index.js
@@ -7,15 +7,27 @@ import { app } from './app.js';
 //     path:'../env'
 // });
 
-connectDB().then(()=>{
-    app.on("error",(error)=>{
-        console.log("Error - ",error);
-        process.exit(1)
-    })
-    app.listen(process.env.PORT||8000,()=>{
-        console.log(`Express is running at PORT : ${process.env.PORT}`);
-    })
-}).catch((err)=>{
-    console.log("Database connection failed :(",err);
-    process.exit(1)
-})
+// connectDB().then(()=>{
+//     app.on("error",(error)=>{
+//         console.log("Error - ",error);
+//         process.exit(1)
+//     })
+//     app.listen(process.env.PORT||8000,()=>{
+//         console.log(`Express is running at PORT : ${process.env.PORT}`);
+//     })
+// }).catch((err)=>{
+//     console.log("Database connection failed :(",err);
+//     process.exit(1)
+// })
+
+let isConnected = false;
+
+
+export default async function handler(req, res) {
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
+  
+    return app(req, res);
+  }
