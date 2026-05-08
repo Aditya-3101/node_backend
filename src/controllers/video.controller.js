bedd3c90f5bd1e0488ceca32804bf16e7137d7ad
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-05-08 12:49:02 +0530

    changed config to test on frontend

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -23,6 +23,8 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
     if(userId) filter.owner=userId
 
+    console.log("COOKIES:", req.cookies);
+
     if(query){
         filter.title = {$regex:query, $options:"i"}
     }
