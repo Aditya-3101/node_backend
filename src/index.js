30b09670a9605b67bfe6fcc65c9ba327331cb5d9
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-05-08 11:06:06 +0530

    db connectivity function rollbacked to previous working version

--- src/index.js
+++ src/index.js
@@ -3,33 +3,33 @@ import connectDB from "./db/index.js";
 import app  from './app.js';
 
 
-// dotenv.config({
-//     path:'../env'
-// });
+dotenv.config({
+    path:'../env'
+});
 
-// connectDB().then(()=>{
-//     app.on("error",(error)=>{
-//         console.log("Error - ",error);
-//         process.exit(1)
-//     })
-//     app.listen(process.env.PORT||8000,()=>{
-//         console.log(`Express is running at PORT : ${process.env.PORT}`);
-//     })
-// }).catch((err)=>{
-//     console.log("Database connection failed :(",err);
-//     process.exit(1)
-// })
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error - ",error);
+        process.exit(1)
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Express is running at PORT : ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("Database connection failed :(",err);
+    process.exit(1)
+})
 
 
-export default async function handler(req, res) {
-  try {
-    if (mongoose.connection.readyState !== 1) {
-      await connectDB();
-    }
+// export default async function handler(req, res) {
+//   try {
+//     if (mongoose.connection.readyState !== 1) {
+//       await connectDB();
+//     }
 
-    return app(req, res);
-  } catch (error) {
-    console.error("DB connection failed:", error);
-    res.status(500).json({ message: "DB connection error" });
-  }
-}
+//     return app(req, res);
+//   } catch (error) {
+//     console.error("DB connection failed:", error);
+//     res.status(500).json({ message: "DB connection error" });
+//   }
+// }
