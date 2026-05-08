5163564052266967227e335ce1d16ea400b26ebd
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-05-08 11:27:29 +0530

    updated api entrypoint for vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
-import app from '../src/app.js';
+// import app from '../src/app.js';
 
-export default app;
+// export default app;
+
+import mongoose from "mongoose";
+import connectDB from "../src/db/index.js";
+import app from "../src/app.js";
+
+export default async function handler(req, res) {
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await connectDB();
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.error("DB ERROR:", error);
+    return res.status(500).json({ message: "DB connection failed" });
+  }
+}
