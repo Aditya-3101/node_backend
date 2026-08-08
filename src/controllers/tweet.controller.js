8bdf0edc4a7ce597a9a387eb9d06d065ff534bf9
Aditya-3101 <adityadhayfule3101@gmail.com>
2026-08-08 20:45:03 +0000

    fixed issue regarding liking a post functionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -54,74 +54,173 @@ const deleteTweet = asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,deletedTweet,"Tweet/post has been deleted successfully"))
 })
 
-const getUserTweets = asyncHandler(async(req,res)=>{
-    const {userId} = req.params
+// const getUserTweets = asyncHandler(async(req,res)=>{
+//     const {userId} = req.params
 
-    if(!isValidObjectId(userId)){
-        throw new ApiError(400,"invalid user id")
-    }
+//     if(!isValidObjectId(userId)){
+//         throw new ApiError(400,"invalid user id")
+//     }
 
-    const loggedInUser = req.user?._id
+//     const loggedInUser = req.user?._id
 
-    if(!loggedInUser){
-        throw new ApiError(401,"user has not logged in")
-    }
+//     if(!loggedInUser){
+//         throw new ApiError(401,"user has not logged in")
+//     }
 
-    const userTweets2 = await Tweet.aggregate([
-        {
-        $match:{
-            owner:new mongoose.Types.ObjectId(userId)
-        }
-    },
+//     const userTweets2 = await Tweet.aggregate([
+//         {
+//         $match:{
+//             owner:new mongoose.Types.ObjectId(userId)
+//         }
+//     },
+//     {
+//         $sort:{
+//             createdAt:-1
+//         }
+//     },
+//     {
+//         $lookup:{
+//             from:"likes",
+//             localField:"_id",
+//             foreignField:"tweet",
+//             as:"tweet_likes"
+//         }
+//     },
+//     {
+//         $addFields:{
+//             likeCount:{$size:"$tweet_likes"},
+//             isLiked: {
+//                 $in: [new mongoose.Types.ObjectId(userId), "$tweet_likes.likedBy"]
+//             }
+//         }
+//     },
+//     {
+//         $lookup:{
+//             from:"users",
+//             localField:"owner",
+//             foreignField:"_id",
+//             as:"user_detail"
+//         }
+//     },
+//     {
+//         $project:{
+//             _id:1,
+//             content:1,
+//             likeCount:1,
+//             isLiked:1,
+//             createdAt:1,
+//             avatar:{$first:"$user_detail.avatar"},
+//             username:{$first:"$user_detail.username"},
+//             owner:{
+//                 id:{$first:"$user_detail._id"}
+//             }
+//         }
+//     }
+//     ])
+
+//     if(!userTweets2){
+//         throw new ApiError(500,"Internal server Error")
+//     }
+
+//     return res.status(200)
+//     .json(new ApiResponse(200,userTweets2,"users posts fetched sucessfully"))
+// })
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "invalid user id");
+  }
+
+  const loggedInUser = req.user?._id;
+
+  if (!loggedInUser) {
+    throw new ApiError(401, "user has not logged in");
+  }
+
+  const userTweets = await Tweet.aggregate([
     {
-        $sort:{
-            createdAt:-1
-        }
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
     },
-    {
-        $lookup:{
-            from:"likes",
-            localField:"_id",
-            foreignField:"tweet",
-            as:"tweet_likes"
-        }
-    },
-    {
-        $addFields:{
-            likeCount:{$size:"$tweet_likes"},
-            isLiked: {
-                $in: [new mongoose.Types.ObjectId(userId), "$tweet_likes.likedBy"]
-            }
-        }
-    },
-    {
-        $lookup:{
-            from:"users",
-            localField:"owner",
-            foreignField:"_id",
-            as:"user_detail"
-        }
-    },
-    {
-        $project:{
-            _id:1,
-            content:1,
-            likeCount:1,
-            isLiked:1,
-            createdAt:1,
-            avatar:{$first:"$user_detail.avatar"},
-            username:{$first:"$user_detail.username"}
-        }
-    }
-    ])
 
-    if(!userTweets2){
-        throw new ApiError(500,"Internal server Error")
-    }
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
 
-    return res.status(200)
-    .json(new ApiResponse(200,userTweets2,"users posts fetched sucessfully"))
-})
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "tweet_likes",
+      },
+    },
+
+    {
+      $addFields: {
+        // Total number of likes
+        likeCount: {
+          $size: "$tweet_likes",
+        },
+        isLiked: {
+          $in: [
+            new mongoose.Types.ObjectId(loggedInUser),
+            "$tweet_likes.likedBy",
+          ],
+        },
+      },
+    },
+
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "user_detail",
+      },
+    },
+
+    {
+      $project: {
+        _id: 1,
+        content: 1,
+        likeCount: 1,
+        isLiked: 1,
+        createdAt: 1,
+
+        avatar: {
+          $first: "$user_detail.avatar",
+        },
+
+        username: {
+          $first: "$user_detail.username",
+        },
+
+        owner: {
+          id: {
+            $first: "$user_detail._id",
+          },
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        userTweets,
+        "users posts fetched successfully"
+      )
+    );
+});
+
 
 const updateTweet = asyncHandler(async(req,res)=>{
     const {tweetId} = req.params
